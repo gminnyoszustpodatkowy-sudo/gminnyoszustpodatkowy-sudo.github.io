c27da49f9cef758053cb4ca0e9b2ee776bd7d9e8
megaultronprivate <gminnyoszustpodatkowy@gmail.com>
2026-03-24 10:47:06 +0100

    s

--- weather-forecast/forecast.js
+++ weather-forecast/forecast.js
@@ -1,4 +1,74 @@
-const api_key = "a9a4c4a33b8f9fde847001f163b57fe4esh9";
+function mapIcon(main){
+    const icons = {
+        Clear: "clear",
+        Cloud: "cloud",
+        Rain: "rain",
+        Thunderstorm: "thunderstorm",
+        Drizzle: "drizlle",
+        Snow: "snow",
+        Mist: "mist",
+    };
+    return icons[main] || "unknown";
+}
+function getUniqueDays(data){
+    const days = new Set();
+    data.list.forEach(item =>{
+        const date = item.dt_txt.split(" ")[0];
+        days.add(date);
+    })
+    return [...days];
+}
+
+function getDayNightForecast(data, date) {
+  const dayHours = ["09:00:00", "12:00:00", "15:00:00", "18:00:00"];
+  const nightHours = ["00:00:00", "03:00:00", "06:00:00", "21:00:00"];
+
+  const dayData = [];
+  const nightData = [];
+
+  data.list.forEach(item => {
+    if (!item.dt_txt.startsWith(date)) return;
+
+    if (dayHours.some(h => item.dt_txt.includes(h))) {
+      dayData.push(item);
+    }
+
+    if (nightHours.some(h => item.dt_txt.includes(h))) {
+      nightData.push(item);
+    }
+  });
+
+  const getMaxTemp = arr =>
+    arr.length ? Math.max(...arr.map(i => i.main.temp)) : null;
+
+  const getIcon = arr => {
+    if (!arr.length) return "unknown";
+
+    const counts = {};
+
+    arr.forEach(i => {
+      const main = i.weather[0].main;
+      counts[main] = (counts[main] || 0) + 1;
+    });
+
+    const mostCommon = Object.keys(counts).reduce((a, b) =>
+      counts[a] > counts[b] ? a : b
+    );
+
+    return mapIcon(mostCommon);
+  };
+
+  return {
+    day: {
+      temp: getMaxTemp(dayData),
+      icon: getIcon(dayData)
+    },
+    night: {
+      temp: getMaxTemp(nightData),
+      icon: getIcon(nightData)
+    }
+  };
+}
 
 function degToDirection(deg) {
     const directions = ["Wieje z północy (N)", "Wieje z północnego wschodu (NE)", "Wieje ze wschodu (E)", "Wieje ze południowego wschodu (SE)", "Wieje z południa (S)", "Wieje z południowego zachodu (SW)", "Wieje z zachodu (W)", "Wieje z północnego zachodu (NW)"];
@@ -11,10 +81,11 @@ function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
+const key = "a9a4c4a33b8f9fde847001f163b57fe4";
+let city = document.querySelector('#city').value;
 
 async function getWeather() {
-    let city = document.querySelector('#city').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${api_key}&units=metric&lang=pl`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}&units=metric&lang=pl`;
     try {
         const response = await fetch(url);
         const data = await response.json();
@@ -22,11 +93,11 @@ async function getWeather() {
             document.querySelector('main').innerHTML = `Nie znaleziono miasta`;
             return;
         }
-        const temp = `${Math.round(Number(data.main.temp))} °C`;
-        const temp_feel = `Odczuwalna: ${Math.round(Number(data.main.feels_like))} °C`;
-        const min_temp = `${Math.round(Number(data.main.temp_min))} °C`;
-        const max_temp = `${Math.round(Number(data.main.temp_max))} °C`;
-        let aside = `↑ ${max_temp}/↓ ${min_temp}`;
+        const temp = `${Math.round(Number(data.main.temp))}°`;
+        const temp_feel = `Odczuwalna: ${Math.round(Number(data.main.feels_like))}°`;
+        const min_temp = `${Math.round(Number(data.main.temp_min))}°`;
+        const max_temp = `${Math.round(Number(data.main.temp_max))}°`;
+        let minmax = `↑ ${max_temp}/↓ ${min_temp}`;
         const pressure = `${data.main.pressure} hPa`;
         const humidity = `${data.main.humidity}%`;
         const mainweather = data.weather[0].main;
@@ -46,49 +117,48 @@ async function getWeather() {
         let city = data.name;
 
         if (min_temp == max_temp) {
-            aside = `↑↓ ${max_temp}`;
+            minmax = `↑↓ ${max_temp}`;
         }
         if (visibility = 10) {
             visibility = `Nieograniczona`;
         }
-        document.querySelector('main').innerHTML = `<section id='weatherCityName'>
+        document.querySelector('main').innerHTML = `<section id='weatherInfo'><section id='weatherCityName'>
         ${city}, ${country}
         </section>
-        <section id='weatherTemperature'>
-        <temperature>
-        ${temp} 
-        </temperature>
-        <feel>
-        ${temp_feel}
-        </feel>
-        <aside>
-        ${aside}
-        </aside>
+        <section id='weatherTemperatureNow'>
+        ${temp}
         </section>
         <section id='weatherName'>
         ${desc}
+        </section><section id='weatherTemperatureNowFeelsLike'> 
+        ${temp_feel}
+        </section><section id='weatherTemperatureMinMax'>
+        ${minmax}
         </section>
-        <section id='weatherPressure'>
-        ${pressure}
+        <section id='weatherForecast'></section>
         </section>
-        <section id='weatherHumidity'>
-        ${humidity}
+        <section id='weather'>
+        <section id='weatherPressure' class='weatherCaffle'><span>Ciśnienie</span><br><h1>
+        ${pressure}</h1>
         </section>
-        <section id='weatherWind>
-        <section id='weatherWindSpeed'>
-        ${wind_speed}
+        <section id='weatherHumidity' class='weatherCaffle'><span>Wilgotność</span><br><h1>
+        ${humidity}</h1>
         </section>
-        <section id='weatherWindDirection'>
-        ${wind_direction}
+        <section id='weatherWind'  class='weatherCaffle'><span>Wiatr</span>
+        <section id='weatherWindSpeed'><h1>
+        ${wind_speed}</h1>
+        </section>
+        <section id='weatherWindDirection'><h1>
+        ${wind_direction}</h1>
         </section>
         </section>
-        <section id='weatherVisibility'>
-        ${visibility}
-        </section>
+        <section id='weatherVisibility' class='weatherCaffle'><span>Widoczność</span><br><h1>
+        ${visibility}</h1>
+        </section> </section>
         <section id='weatherRadar'>
         <a href='https://openweathermap.org/weathermap'>Radar <sup>↗</sup></a>
         </section>
-        <section id='weatherFullGetTime'>${full_time}</section><img src='icons/${icon}.png'>`;
+        <section id='weatherFullGetTime'>${full_time}</section>`;
     }
     catch (error) {
         document.querySelector('main').innerHTML = `Błąd pobierania danych`;
@@ -96,3 +166,44 @@ async function getWeather() {
 }
 
 getWeather();
+
+async function getForecast() {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${key}&units=metric&lang=pl`;
+
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (data.cod !== "200") {
+            document.querySelector('#weatherForecast').innerHTML = "Błąd miasta";
+            return;
+        }
+
+        const days = getUniqueDays(data);
+
+        const forecast = days.map(day => ({
+            date: day,
+            ...getDayNightForecast(data, day)
+        }));
+
+        const result = document.querySelector('#weatherForecast');
+        result.innerHTML = "";
+
+        forecast.slice(0, 5).forEach(item => {
+            result.innerHTML += `
+                <div id="day">
+                    <span>${item.date}</span>
+                    <span><img src='icons/${item.day.icon}.png'></span>
+                    <span>
+                        ${Math.round(item.day.temp)}° / ${Math.round(item.night.temp)}°
+                    </span>
+                </div>
+            `;
+        });
+
+    } catch (e) {
+        document.querySelector('#weatherForecast').innerHTML = "Błąd pobierania";
+    }
+}
+
+getForecast()
